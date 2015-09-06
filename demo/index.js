c510caa578fd9ae6f8ec094f5269653a6b4dc19e
张顺 <zhangshun@alipay.com>
2015-09-06 17:37:08 +0800

    fastclick单词错误

--- demo/index.js
+++ demo/index.js
@@ -5,7 +5,7 @@
  * Copyright 2014-2015, Tingle Team, Alinw.
  * All rights reserved.
  */
-window.FaskClick && FastClick.attach(document.body);
+window.FastClick && FastClick.attach(document.body);
 require("tingle-context");
 
 const Demo = require('./TextareaFieldDemo');
